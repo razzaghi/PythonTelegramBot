b89be1e3e017a2f734216d77ab467081c7f3b7b2
Mr <razzaghi229@gmail.com>
2015-08-30 16:23:42 +0400

    Add JarFile

--- jjoBot.py
+++ jjoBot.py
@@ -305,12 +305,5 @@ def command_default(m):
         print(inst.args)      # arguments stored in .args
         print(inst)           # __str__ allows args to be printed directly
 
-
-
 while True:
-    try:
         time.sleep(1000)
-    except Exception as inst:
-        print(type(inst))     # the exception instance
-        print(inst.args)      # arguments stored in .args
-        print(inst)           # __str__ allows args to be printed directly
